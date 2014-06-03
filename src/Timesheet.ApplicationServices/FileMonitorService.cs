ebb65bc465d486479a2a42f0784409a9b8e57d6c
Tom Marien <tommarien@gmail.com>
2014-06-03 14:10:31 +0200

    Added Monitor Topshelf Service

---
+++ src/Timesheet.ApplicationServices/FileMonitorService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+namespace Timesheet.ApplicationServices
+{
+    public class FileMonitorService
+    {
+        private readonly FileSystemWatcher _fileSystemWatcher;
+
+        public FileMonitorService(string path, string filter)
+        {
+            _fileSystemWatcher = new FileSystemWatcher(path, filter)
+            {
+                NotifyFilter = NotifyFilters.FileName
+                               | NotifyFilters.DirectoryName
+                               | NotifyFilters.LastWrite
+                               | NotifyFilters.LastAccess
+                               | NotifyFilters.Attributes
+            };
+
+            _fileSystemWatcher.Changed += (sender, e) => Console.WriteLine(e.FullPath);
+            _fileSystemWatcher.Deleted += (sender, e) => Console.WriteLine(e.FullPath + " Deleted");
+        }
+
+        public void Start()
+        {
+            _fileSystemWatcher.EnableRaisingEvents = true;
+        }
+
+        public void Stop()
+        {
+            _fileSystemWatcher.EnableRaisingEvents = false;
+        }
+    }
+}
---
+++ src/Timesheet.Monitor/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Configuration;
+using Timesheet.ApplicationServices;
+using Topshelf;
+
+namespace Timesheet.Monitor
+{
+    internal class Program
+    {
+        private static int Main(string[] args)
+        {
+            TopshelfExitCode exitCode = HostFactory.Run(
+                config =>
+                {
+                    config.Service<TimesheetMonitorService>(
+                        s =>
+                        {
+                            s.ConstructUsing(f => new TimesheetMonitorService(
+                                ConfigurationManager.AppSettings["Monitor.Path"]
+                                , ConfigurationManager.AppSettings["Monitor.Filter"]));
+                            s.WhenStarted(f => f.Start());
+                            s.WhenStopped(f => f.Stop());
+                        });
+
+
+                    config.RunAsLocalSystem();
+                    config.SetServiceName("TimeSheetMonitor");
+                    config.SetDisplayName("Timesheet monitor");
+                });
+
+            return (int) exitCode;
+        }
+    }
+}
